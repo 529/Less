f477a5df511a5818d5e03c18c9908aa22513f66e
529 <koji.sakata@gmail.com>
2015-02-06 23:41:44 +0900

    modify

--- index.php
+++ index.php
@@ -16,214 +16,214 @@
 
 
 <?php
-	/*-----------------------------------------------------------------------------------*/
-	/* Start header
-	/*-----------------------------------------------------------------------------------*/
+    /*-----------------------------------------------------------------------------------*/
+    /* Start header
+    /*-----------------------------------------------------------------------------------*/
 ?>
 
 <header id="masthead" class="site-header" role="banner">
-	<div class="container">
-		
-		<div class="gravatar">
-			<?php 
-				// grab admin email and their photo
-				$admin_email = get_option('admin_email');
-				echo get_avatar( $admin_email, 100 ); 
-			?>
-		</div><!--/ author -->
-		
-		<div id="brand">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
-		</div><!-- /brand -->
-	
-		<nav role="navigation" class="site-navigation main-navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- .site-navigation .main-navigation -->
-		
-		<div class="clear"></div>
-	</div><!--/container -->
-		
+    <div class="container">
+        
+        <div class="gravatar">
+            <?php 
+                // grab admin email and their photo
+                $admin_email = get_option('admin_email');
+                echo get_avatar( $admin_email, 100 ); 
+            ?>
+        </div><!--/ author -->
+        
+        <div id="brand">
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
+        </div><!-- /brand -->
+    
+        <nav role="navigation" class="site-navigation main-navigation">
+            <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+        </nav><!-- .site-navigation .main-navigation -->
+        
+        <div class="clear"></div>
+    </div><!--/container -->
+        
 </header><!-- #masthead .site-header -->
 
 <div class="container">
 
-	<div id="primary">
-		<div id="content" role="main">
+    <div id="primary">
+        <div id="content" role="main">
 
 
 <?php
-	/*-----------------------------------------------------------------------------------*/
-	/* Start Home loop
-	/*-----------------------------------------------------------------------------------*/
-	
-	if( is_home() || is_archive() ) {
-	
+    /*-----------------------------------------------------------------------------------*/
+    /* Start Home loop
+    /*-----------------------------------------------------------------------------------*/
+    
+    if( is_home() || is_archive() ) {
+    
 ?>
-			<?php if ( have_posts() ) : ?>
+            <?php if ( have_posts() ) : ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
 
-					<article class="post">
-					
-						<h1 class="title">
-							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-								<?php the_title() ?>
-							</a>
-						</h1>
-						<div class="post-meta">
-							<?php if( comments_open() ) : ?>
-								<span class="comments-link">
-									<?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
-								</span>
-							<?php endif; ?>
-						
-						</div><!--/post-meta -->
-						
-						<div class="the-content">
-							<?php the_content( 'Continue...' ); ?>
-							
-							<?php wp_link_pages(); ?>
-						</div><!-- the-content -->
-						
-						<div class="meta clearfix">
-							<div class="category"><?php echo get_the_category_list(); ?></div>
-							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
-						</div><!-- Meta -->
-						
-					</article>
+                    <article class="post">
+                    
+                        <h1 class="title">
+                            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                <?php the_title() ?>
+                            </a>
+                        </h1>
+                        <div class="post-meta">
+                            <?php if( comments_open() ) : ?>
+                                <span class="comments-link">
+                                    <?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
+                                </span>
+                            <?php endif; ?>
+                        
+                        </div><!--/post-meta -->
+                        
+                        <div class="the-content">
+                            <?php the_content( 'Continue...' ); ?>
+                            
+                            <?php wp_link_pages(); ?>
+                        </div><!-- the-content -->
+                        
+                        <div class="meta clearfix">
+                            <div class="category"><?php echo get_the_category_list(); ?></div>
+                            <div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
+                        </div><!-- Meta -->
+                        
+                    </article>
 
-				<?php endwhile; ?>
-				
-				<!-- pagintation -->
-				<div id="pagination" class="clearfix">
-					<div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
-					<div class="next-page"><?php next_posts_link( ' &laquo; Older' ); ?></div>
-				</div><!-- pagination -->
+                <?php endwhile; ?>
+                
+                <!-- pagintation -->
+                <div id="pagination" class="clearfix">
+                    <div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
+                    <div class="next-page"><?php next_posts_link( ' &laquo; Older' ); ?></div>
+                </div><!-- pagination -->
 
 
-			<?php else : ?>
-				
-				<article class="post error">
-					<h1 class="404">Nothing posted yet</h1>
-				</article>
+            <?php else : ?>
+                
+                <article class="post error">
+                    <h1 class="404">Nothing posted yet</h1>
+                </article>
 
-			<?php endif; ?>
+            <?php endif; ?>
 
-		
-	<?php } //end is_home(); ?>
+        
+    <?php } //end is_home(); ?>
 
 <?php
-	/*-----------------------------------------------------------------------------------*/
-	/* Start Single loop
-	/*-----------------------------------------------------------------------------------*/
-	
-	if( is_single() ) {
+    /*-----------------------------------------------------------------------------------*/
+    /* Start Single loop
+    /*-----------------------------------------------------------------------------------*/
+    
+    if( is_single() ) {
 ?>
 
 
-			<?php if ( have_posts() ) : ?>
+            <?php if ( have_posts() ) : ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
 
-					<article class="post">
-					
-						<h1 class="title"><?php the_title() ?></h1>
-						<div class="post-meta">
-							<?php if( comments_open() ) : ?>
-								<span class="comments-link">
-									<?php comments_popup_link( __( 'Comment', 'less' ), __( '1 Comment', 'less' ), __( '% Comments', 'less' ) ); ?>
-								</span>
-							<?php endif; ?>
-						
-						</div><!--/post-meta -->
-						
-						<div class="the-content">
-							<?php the_content( 'Continue...' ); ?>
-							
-							<?php wp_link_pages(); ?>
-						</div><!-- the-content -->
-						
-						<div class="meta clearfix">
-							<div class="category"><?php echo get_the_category_list(); ?></div>
-							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
-						</div><!-- Meta -->						
-						
-					</article>
+                    <article class="post">
+                    
+                        <h1 class="title"><?php the_title() ?></h1>
+                        <div class="post-meta">
+                            <?php if( comments_open() ) : ?>
+                                <span class="comments-link">
+                                    <?php comments_popup_link( __( 'Comment', 'less' ), __( '1 Comment', 'less' ), __( '% Comments', 'less' ) ); ?>
+                                </span>
+                            <?php endif; ?>
+                        
+                        </div><!--/post-meta -->
+                        
+                        <div class="the-content">
+                            <?php the_content( 'Continue...' ); ?>
+                            
+                            <?php wp_link_pages(); ?>
+                        </div><!-- the-content -->
+                        
+                        <div class="meta clearfix">
+                            <div class="category"><?php echo get_the_category_list(); ?></div>
+                            <div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
+                        </div><!-- Meta -->                        
+                        
+                    </article>
 
-				<?php endwhile; ?>
-				
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template( '', true );
-				?>
-
-
-			<?php else : ?>
-				
-				<article class="post error">
-					<h1 class="404">Nothing posted yet</h1>
-				</article>
-
-			<?php endif; ?>
+                <?php endwhile; ?>
+                
+                <?php
+                    // If comments are open or we have at least one comment, load up the comment template
+                    if ( comments_open() || '0' != get_comments_number() )
+                        comments_template( '', true );
+                ?>
 
 
-	<?php } //end is_single(); ?>
-	
+            <?php else : ?>
+                
+                <article class="post error">
+                    <h1 class="404">Nothing posted yet</h1>
+                </article>
+
+            <?php endif; ?>
+
+
+    <?php } //end is_single(); ?>
+    
 <?php
-	/*-----------------------------------------------------------------------------------*/
-	/* Start Page loop
-	/*-----------------------------------------------------------------------------------*/
-	
-	if( is_page()) {
+    /*-----------------------------------------------------------------------------------*/
+    /* Start Page loop
+    /*-----------------------------------------------------------------------------------*/
+    
+    if( is_page()) {
 ?>
 
-			<?php if ( have_posts() ) : ?>
+            <?php if ( have_posts() ) : ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
 
-					<article class="post">
-					
-						<h1 class="title"><?php the_title() ?></h1>
-						
-						<div class="the-content">
-							<?php the_content(); ?>
-							
-							<?php wp_link_pages(); ?>
-						</div><!-- the-content -->
-						
-					</article>
+                    <article class="post">
+                    
+                        <h1 class="title"><?php the_title() ?></h1>
+                        
+                        <div class="the-content">
+                            <?php the_content(); ?>
+                            
+                            <?php wp_link_pages(); ?>
+                        </div><!-- the-content -->
+                        
+                    </article>
 
-				<?php endwhile; ?>
+                <?php endwhile; ?>
 
-			<?php else : ?>
-				
-				<article class="post error">
-					<h1 class="404">Nothing posted yet</h1>
-				</article>
+            <?php else : ?>
+                
+                <article class="post error">
+                    <h1 class="404">Nothing posted yet</h1>
+                </article>
 
-			<?php endif; ?>
+            <?php endif; ?>
 
-	<?php } // end is_page(); ?>
+    <?php } // end is_page(); ?>
 
-		</div><!-- #content .site-content -->
-	</div><!-- #primary .content-area -->
+        </div><!-- #content .site-content -->
+    </div><!-- #primary .content-area -->
 
 </div><!-- / container-->
 
 <?php
-	/*-----------------------------------------------------------------------------------*/
-	/* Start Footer
-	/*-----------------------------------------------------------------------------------*/
+    /*-----------------------------------------------------------------------------------*/
+    /* Start Footer
+    /*-----------------------------------------------------------------------------------*/
 ?>
 
 <footer class="site-footer" role="contentinfo">
-	<div class="site-info container">
-		<?php do_action( 'break_credits' ); ?>
-		<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
-		<span class="sep"> and </span>
-		<a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
-	</div><!-- .site-info -->
+    <div class="site-info container">
+        <?php do_action( 'break_credits' ); ?>
+        <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
+        <span class="sep"> and </span>
+        <a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
+    </div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
 
 <?php wp_footer(); ?>
